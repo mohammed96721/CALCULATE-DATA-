27594f6ac0916764a1691d25eef0da0d1d481907
mohammed96721 <42373419+mohammed96721@users.noreply.github.com>
2025-05-23 01:40:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const cors = require('cors'); // أضف هذه السطر
 const app = express();
+
+// تمكين CORS
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static('public'));
