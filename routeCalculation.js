313bfe3620fb1f1df9618ee5cb3f235381f6655c
mohammed96721 <42373419+mohammed96721@users.noreply.github.com>
2025-05-24 23:44:32 +0300

    Update and rename routeCalculation.js to 0

--- routeCalculation.js
+++
@@ -1,6 +0,0 @@
-function preProcess(data) {
-  console.log('البيانات في routeCalculation.js:', data); // تسجيل
-  return data; // إرجاع البيانات كما هي للاختبار
-}
-
-module.exports = { preProcess };
