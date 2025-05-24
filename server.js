56bcb659d8d7c470b58529becd0dcff62b06c193
mohammed96721 <42373419+mohammed96721@users.noreply.github.com>
2025-05-24 03:10:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const middleware = require('./middleware');
+const calculate = require('./api/calculate');
+
+dotenv.config();
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.post('/api/calculate', require('./api/calculate'));
+// تطبيق middleware على المسار
+app.post('/api/calculate', middleware, calculate);
 
 module.exports.handler = serverless(app);
